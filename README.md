1934a3e1c381fba7f1e3204992784c988a0e9e02
Jose Padilla <jose.padilla.dev@gmail.com>
2025-06-10 08:55:15 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Instructions
+This script will download, verify and uncompress MediaMTX binaries
+[MediaMTX repository](https://github.com/bluenviron/mediamtx?tab=readme-ov-file#standalone-binary)
+---
+
+## Clone this repository
+---
+
+## Make the script executable
+```bash
+chmod +x install_mediamtx.sh
+```
+---
+
+## Start the server
+```bash
+./mediamtx
+```
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define version and filenames
+FILE="mediamtx_v1.12.3_linux_arm64.tar.gz"
+SUMFILE="mediamtx_v1.12.3_linux_arm64.tar.gz.sha256sum"
+BASE_URL="https://github.com/bluenviron/mediamtx/releases/download/v1.12.3"
+
+# Download the tarball and the checksum
+echo "Downloading files..."
+wget -q --show-progress "${BASE_URL}/${FILE}"
+wget -q --show-progress "${BASE_URL}/${SUMFILE}"
+
+# Verify the SHA256 checksum
+echo "Verifying checksum..."
+if sha256sum -c "${SUMFILE}"; then
+    echo "Checksum verified."
+    
+    # Decompress the archive
+    echo "Decompressing..."
+    tar -xvzf "${FILE}"
+    
+    echo "Done."
+else
+    echo "Checksum verification failed. Exiting."
+    exit 1
+fi
+
